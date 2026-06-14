e43c486096a589ae77d1d1bf207a8eed25609689
nadasoliman2 <nadaemad271@gmail.com>
2026-06-14 16:57:23 +0300

    updates

--- app/_components/chatbot.tsx
+++ app/_components/chatbot.tsx
@@ -33,7 +33,7 @@ export default function Chatbot() {
   useEffect(() => {
     if (containerRef.current && typeof window !== "undefined") {
       const widget = document.createElement("elevenlabs-convai");
-      widget.setAttribute("agent-id", "agent_6701krcdzz72e9k9fdsxr9va0xgy");
+      widget.setAttribute("agent-id", "agent_2701ks0en06jf80v8f6a4zbk733j");
       widget.setAttribute("style", "width: 350px; height: 500px;");
       containerRef.current.appendChild(widget);
 
